dd16bdf1471411e8df7a21a50c2fb587f1aec0b9
Razr <48860184+finley541@users.noreply.github.com>
2026-06-30 19:33:17 -0400

    Added Bloxburg. Updated the games file to match the new naming.

--- Games.lua
+++ Games.lua
@@ -6,11 +6,16 @@ return {
 
     [286090429] = {
         Name = "Arsenal",
-        Url = "https://raw.githubusercontent.com/finley541/a/refs/heads/main/Arsenal.lua",
+        Url = "https://raw.githubusercontent.com/finley541/a/refs/heads/main/Arsenal_.lua",
+    },
+
+    [185655149] = {
+        Name = "Bloxburg",
+        Url = "https://raw.githubusercontent.com/finley541/a/refs/heads/main/Bloxburg_.lua"
     },
 
     [142823291] = {
         Name = "Murder Mystery 2",
-        Url = "https://raw.githubusercontent.com/finley541/a/refs/heads/main/MM2.lua",
-    },
+        Url = "https://raw.githubusercontent.com/finley541/a/refs/heads/main/MM2_.lua",
+    }
 }
